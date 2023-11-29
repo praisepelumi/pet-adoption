ea610c33c9718dd04b99d04db7c29519c1271062
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-11-29 18:38:55 -0500

    Added more search fields

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,14 +1,58 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const BREED = [];
+
 const SearchParams = () => {
-  const location = "New York, NY";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-          <input type="text" id='location' value={location} placeholder="Location" />
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return <option key={animal}>{animal}</option>;
+            })}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            id="breed"
+            value={breed}
+            placeholder="Breed"
+          >
+            <option />
+            {BREED.map((breed) => {
+              return <option key={breed}>{breed}</option>;
+            })}
+          </select>
+        </label>
+        <button>Submut</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;

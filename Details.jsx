34786b81245629ff8d3a8aba18a7c4a20fc526d0
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-12-01 15:09:44 -0500

    made header a link &utilized useParams to get id

--- Details.jsx
+++ Details.jsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
+// browser routers stores the parameters and we utilize useParams to access it
+
 const Details = () => {
-  return <h2>hi!</h2>;
+  const { id } = useParams();
+  return <h2>{id}</h2>;
 };
 
 export default Details;
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 /* eslint-env browser */
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "../Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />

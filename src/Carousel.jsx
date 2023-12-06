6d814b17ff5d94652759f2079e5cbb5c2152e7af
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-12-05 21:51:21 -0500

    update profile photo based on current photo

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,12 @@ class Carousel extends Component {
     images: ["https://pets-images.dev-apis.com/pets/none.jpg"]
   };
 
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -19,7 +25,10 @@ class Carousel extends Component {
         <div className="carousel-smaller">
           {images.map((photo, index) => {
             return (
+              // eslint-disable-next-line
               <img
+                onClick={this.handleIndexClick}
+                data-index={index}
                 key={photo}
                 src={photo}
                 className={index === active ? "active" : ""}

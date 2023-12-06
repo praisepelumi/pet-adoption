58342f629f62fbee7de3c0d3125dda1f9b7688ac
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-12-06 15:20:12 -0500

    implementing context for managing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 /* eslint-env browser */
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ApotedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,16 +18,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <ApotedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </ApotedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
